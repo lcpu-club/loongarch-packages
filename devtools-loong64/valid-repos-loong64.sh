686fa703ac97da846690e059d5123599a1a4cfa5
wszqkzqk <wszqkzqk@qq.com>
2024-09-07 17:09:36 +0800

    addpkg: devtools-loong64, ver=1.2.1.patch7-2 (#46)
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

---
+++ devtools-loong64/valid-repos-loong64.sh
@@ -0,0 +1,16 @@
+#!/hint/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+:
+
+# shellcheck disable=2034
+DEVTOOLS_VALID_REPOS=(
+        core core-staging core-testing
+        extra extra-staging extra-testing
+)
+
+# shellcheck disable=2034
+DEVTOOLS_VALID_BUILDREPOS=(
+        core-staging core-testing
+        extra extra-staging extra-testing
+)
